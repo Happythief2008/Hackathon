08a86a79280adffc3a29b079362feec37f3b95dd
happythief08 <jjy080803@gmail.com>
2025-07-18 17:51:59 +0900

    플레이어 공격 테스트

--- Assets/Scripts/Player_State.cs
+++ Assets/Scripts/Player_State.cs
@@ -4,9 +4,9 @@ public class Player_State : MonoBehaviour
 {
     public int Hp = 1000;
     public int Damage = 10;
-    public float shootingSpeed = 5; // 총알 속도
-    public float shootingCooltime = 0.5f; // 공격 속도
-    public float intersection = 0; // 사거리
+    public float shootingSpeed = 30; // 총알 속도
+    public float shootingCooltime = 0.3f; // 공격 속도
+    public float intersection = 10; // 사거리
     public float dashspeed = 10; 
     public float dashcoolTime = 0.3f;
     public float PlayerMoveSpeed = 9;
