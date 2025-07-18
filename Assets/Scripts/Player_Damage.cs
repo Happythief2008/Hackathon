df3397c5f2f6bbeebc705f7cd1a7bcc75fc8adc2
happythief08 <jjy080803@gmail.com>
2025-07-19 04:06:56 +0900

    플레이어 피격 애니메이션 제작

--- Assets/Scripts/Player_Damage.cs
+++ Assets/Scripts/Player_Damage.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
-using System.Collections;
 
 public class Player_Damage : MonoBehaviour
 {
     Player_State state;
+    Animator animator;
     public Transform Enemy;
 
     void Start()
     {
         state = GetComponent<Player_State>();
+        animator = GetComponent<Animator>();
         Enemy = GameObject.FindWithTag("Enemy").transform;
     }
 
@@ -17,15 +18,17 @@ public class Player_Damage : MonoBehaviour
         state.Hp -= Damage;
         Debug.Log($"{gameObject.name}이(가) {Damage}의 데미지를 받았습니다. 현재 체력: {state.Hp}");
 
+        animator.SetTrigger("Hit");
+
         if (state.Hp <= 0)
         {
             Die();
         }
     }
+
     void Die()
     {
         Debug.Log($"{gameObject.name}이(가) 사망했습니다.");
-
         Destroy(gameObject);
     }
 }
